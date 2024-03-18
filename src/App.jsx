55313b6221d0e77463b79b7fd62c2560b165717d
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-18 15:03:13 -0400

    Create input heading and button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+    background-color: orangered;
+    padding: 20px;
+`;
 
 function App() {
     return (
         <>
             <GlobalStyles />
-            <div>Hello World</div>;
+            <StyledApp>
+                <Heading as="h1">The Wild Oasis</Heading>
+            </StyledApp>
         </>
     );
 }
