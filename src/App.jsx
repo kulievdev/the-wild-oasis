57f4425cd3765bf987cfe905199bbd1705e848e5
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-18 15:22:27 -0400

    Build row, and improve button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const StyledApp = styled.main`
-    background-color: orangered;
     padding: 20px;
 `;
 
@@ -12,7 +14,41 @@ function App() {
         <>
             <GlobalStyles />
             <StyledApp>
-                <Heading as="h1">The Wild Oasis</Heading>
+                <Row type="vertical">
+                    <Row type="horizontal">
+                        <Heading as="h1">The Wild Oasis</Heading>
+                        <div>
+                            <Heading as="h2">Check in and out</Heading>
+                            <Button
+                                variation="primary"
+                                onClick={() => alert("Check in")}
+                            >
+                                Check in
+                            </Button>
+                            <Button
+                                variation="secondary"
+                                size="small"
+                                onClick={() => alert("Check out")}
+                            >
+                                Check out
+                            </Button>
+                        </div>
+                    </Row>
+
+                    <Row type="vertical">
+                        <Heading as="h3">Form</Heading>
+                        <form>
+                            <Input
+                                type="number"
+                                placeholder="Number of guests"
+                            ></Input>
+                            <Input
+                                type="number"
+                                placeholder="Number of guests"
+                            ></Input>
+                        </form>
+                    </Row>
+                </Row>
             </StyledApp>
         </>
     );
